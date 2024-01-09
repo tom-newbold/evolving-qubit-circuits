47b8c87191eeabd94576670efff1ce0ea6c060dc
tom-newbold <thomas_newbold@outlook.com>
2024-01-09 10:28:08 +0000

    added circuit check

--- toffoli_gate_generation.py
+++ toffoli_gate_generation.py
@@ -34,8 +34,38 @@ if __name__=="__main__":
                 {'label':'cphase','inputs':2,'parameters':1}]
     
     TOFFOLI = ToffoliGeneration(GATE_SET)
-    E = Evolution(TOFFOLI)
+    E = Evolution(TOFFOLI)#, number_of_generations=10)
     
     #population = E.random_search()
     #population = E.stochastic_hill_climb()
     population = E.evolutionary_search()
+
+    ### --- check best circuit ---
+    from qiskit.quantum_info import Operator
+    import numpy as np
+
+    input_states = [list_to_state(x) for x in TOFFOLI.toffoli_inputs]
+    output_states = [list_to_state(y) for y in TOFFOLI.toffoli_outputs]
+
+    c = population[0].to_circuit()
+    differences = {'global':0,'local':0}
+    M = Operator(c)
+    for i in range(len(input_states)):
+        state = input_states[i]
+        calc_state = state.evolve(M)
+        print(f"target: {output_states[i].draw(output='latex_source')} --> actual: {calc_state.draw(output='latex_source')}")
+        #output_states[i].draw(output='latex')
+        if calc_state==output_states[i]:
+            #print('exacly correct')
+            pass
+        else:
+            if abs(np.inner(output_states[i].data, calc_state.data).item())**2 == 1.0:
+                print('state correct up to global phase')
+                differences['global'] += 1
+            else:
+                #print('state incorrect')
+                differences['local'] += 1
+
+    print(f"exactly correct for {8-differences['global']-differences['local']} states")
+    if differences['global']+differences['local'] != 0:
+        print(f"correct (up to global phase) for {8-differences['local']} states")
