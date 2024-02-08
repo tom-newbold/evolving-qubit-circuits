ebac69027af25ce37d45ad56fee43decae5634d3
tom-newbold <thomas_newbold@outlook.com>
2024-02-08 16:29:33 +0000

    testing many runs of one param set

--- quantum_fourier_transform_grid_search.py
+++ quantum_fourier_transform_grid_search.py
@@ -10,5 +10,6 @@ if __name__=="__main__":
     E = Evolution(QFT_GEN, sample_percentage=0.1, gen_mulpilier=2.5)
     
     null_f = QFT_GEN.get_null_circuit_fitness()
+    #grid_search(E, 10, [0], [0], [0.1])
     grid_search(E, 1, [0], [0], [0.05,0.1,0.15,0.2,0.25], [0.025,0.1,0.5]) # currently not iterating random sample size
     # try multiples of null fitness
