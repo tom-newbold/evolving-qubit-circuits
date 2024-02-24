dadaeff36d115732d867a257f79984b31fe7b0be
tom-newbold <thomas_newbold@outlook.com>
2024-02-24 00:13:05 +0000

    code cleanup and comments

--- box_plot.py
+++ box_plot.py
@@ -1,39 +1,31 @@
-from pandas import DataFrame, read_csv
+from pandas import read_csv
 import matplotlib.pyplot as plt
-#TEST_PARAMETERS = [['random','stochastic','evolution'],
-#                   ['reduced','overcomplete'],
-#                   [f'{q}qubits' for q in [3,4,5]],
-#                   [f'crossover{c}insertdelete{i}' for i in [1] for c in [3,4,5,6,7]]]#for x in ['single','double']
 
 if __name__=="__main__":
     filepath = 'out/autosave_test_2'
     with open(filepath+'/params.txt','r') as file:
+        # fetches run parameters in order to consruct csv filenames
         lines = [l.strip('\n') for l in file.readlines()]
         ITERATIONS = int(lines[0])
         multipliers = [int(m) for m in lines[1].split(',')]
         test_params = lines[2].split(',')
-    #ITERATIONS = 10
-    #multipliers = [5]#[2,4,8]
 
     csv_to_plot = [f'{tp}_mult{m}_boxplot.csv' for m in multipliers for tp in test_params]
-    #for tp in test_params[3]:
-    #    for m in multipliers:
-    #        csv_to_plot.append(f"{tp}_mult{m}_boxplot.csv")
-    #csv_to_plot = ["5iterations_reduced_mult2.csv","5iterations_reduced_mult4.csv",
-    #               "5iterations_overcomplete_mult2.csv","5iterations_overcomplete_mult4.csv"]
-    
     columns_to_plot = [("peak_fitness",[0,1]),("generations_taken_to_converge",[0,50]),("runtime",[]),("peak_fitness/runtime",[])]
+    # extracts dataframes
     dataframes = [read_csv(filepath+'/'+csv_filename) for csv_filename in csv_to_plot]
 
-    STEPS = 10
+
     for c, r in columns_to_plot:
         if False in [c in d for d in dataframes] and '/' not in c:
+            # checks that column exisits in every dataframe, this is skiped for compound keys (division)
             continue
         #if False in [c.split('/')[0] in d for d in dataframes]+[c.split('/')[1] in d for d in dataframes]:
         #    continue
         data = []
         labels = []
         for i, d in enumerate(dataframes):
+            # for each row, add data and format column label
             if '/' in c:
                 c = c.split('/')
                 data.append(d[c[0]]/d[c[1]])
@@ -45,11 +37,12 @@ if __name__=="__main__":
         plt.clf()
         plt.title(' '.join(c.split('_')))
         if len(r)!=0:
+            # set vertical limit/ticks if range is provided
             plt.ylim(r)
-            plt.yticks([r[0] + i*(r[1]-r[0])/STEPS for i in range(STEPS+1)])
-        plt.xticks(rotation=20)
+            plt.yticks([r[0] + i*(r[1]-r[0])/10 for i in range(11)])
+        plt.xticks(rotation=20) # orient column labels
         plt.boxplot(data, labels=labels)
         plt.grid(axis='y')
-        plt.tight_layout()
-        #plt.show()
+        plt.tight_layout() # fit to labels
         plt.savefig(f'{filepath}/{c.replace("/","_")}_boxplot.png')
+        #plt.show()
