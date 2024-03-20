e9dcefd7e8582618b2d56abaf07c0d0ad604a85e
tom-newbold <thomas_newbold@outlook.com>
2024-03-20 16:09:19 +0000

    final parameters

--- gecco_experiments.py
+++ gecco_experiments.py
@@ -17,28 +17,30 @@ def run_alg_test_reduced(e_instance,m=6):
     for test_param in list(stats.keys()):
         e_instance.output(plot, stats, test_param, m)
 
-ITERATIONS = 10
-GENERATIONS = 50
+ITERATIONS = 50
+GENERATIONS = 100
+BOUNDS = [15,45]
+MULTIPLIER = 8
 if __name__=="__main__":
     base_filepath="out/gecco_local"
     #['random','evolution']
     # toffoli
-    for qubit_count in [3]:#[3,4,5,6]:
+    for qubit_count in [3,4,5]:
         print(f'toffoli{qubit_count}')
         PROB_PARAM = AppliedProblemParameters(GATE_SET, genericToffoliConstructor(qubit_count),
-                                              genotype_len_bounds=[15,45], genotype_length_falloff='linear')
+                                              genotype_len_bounds=BOUNDS, genotype_length_falloff='linear')
         experiment_instance = Experiments(PROB_PARAM,iterations=ITERATIONS,save_filepath=f'{base_filepath}/toffoli_{qubit_count}qubit',
-                                          generation_count=GENERATIONS,multipliers=[6])
+                                          generation_count=GENERATIONS,multipliers=[MULTIPLIER])
         #experiment_instance.run_test('algorithm')
-        run_alg_test_reduced(experiment_instance, 6)
+        run_alg_test_reduced(experiment_instance, MULTIPLIER)
         boxplot_from_folder(f'{base_filepath}/toffoli_{qubit_count}qubit', fitness_reference=(2**qubit_count-1)/(2**qubit_count))
     # qft
-    for qubit_count in [3]:#[2,3,4,5]:
+    for qubit_count in [2,3,4,5]:
         print(f'qft{qubit_count}')
         PROB_PARAM = AppliedProblemParameters(GATE_SET, QFT_blueprint(qubit_count),
-                                              genotype_len_bounds=[15,45], genotype_length_falloff='linear')
+                                              genotype_len_bounds=BOUNDS, genotype_length_falloff='linear')
         experiment_instance = Experiments(PROB_PARAM,iterations=ITERATIONS,save_filepath=f'{base_filepath}/qft_{qubit_count}qubit',
-                                          generation_count=GENERATIONS,multipliers=[6])
+                                          generation_count=GENERATIONS,multipliers=[MULTIPLIER])
         #experiment_instance.run_test('algorithm')
-        run_alg_test_reduced(experiment_instance, 6)
+        run_alg_test_reduced(experiment_instance, MULTIPLIER)
         boxplot_from_folder(f'{base_filepath}/qft_{qubit_count}qubit', fitness_reference=(2**qubit_count-1)/(2**qubit_count))
