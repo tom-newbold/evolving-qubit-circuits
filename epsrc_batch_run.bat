5b04d9c2068a5310af3dd05db30e8a798cbe20d1
tom-newbold <thomas_newbold@outlook.com>
2024-07-31 13:59:35 +0100

    epsrc_experiments now runs with sys argv for batch job

---
+++ epsrc_batch_run.bat
@@ -0,0 +1,6 @@
+python epsrc_experiments.py qft 3
+python epsrc_experiments.py qft 4
+python epsrc_experiments.py qft 5
+python epsrc_experiments.py toffoli 3
+python epsrc_experiments.py toffoli 4
+python epsrc_experiments.py toffoli 5
--- epsrc_experiments.py
+++ epsrc_experiments.py
@@ -3,12 +3,36 @@ from quantum_fourier_transform import QFTGeneration, GATE_SET
 from toffoli_gate_generation import ToffoliGeneration
 from box_plot import boxplot_from_folder
 
-if __name__=="__main__":
-    folder = 'out/eprsc_optimisers'
+import sys
 
-    QFT_GEN = QFTGeneration(GATE_SET, 3)
-    experiment_instance = Experiments(QFT_GEN,iterations=20,multipliers=[8],generation_count=100,
+if __name__=="__main__":
+    #print(sys.argv)
+    # sys.argv = [filepath, algorithm, qubits, output_directory]
+    if len(sys.argv)>=3:
+        try:
+            N = int(sys.argv[2])
+            if N > 0 and N < 10:
+                if sys.argv[1]=='qft':
+                    APP = QFTGeneration(GATE_SET, N)
+                elif sys.argv[1]=='toffoli':
+                    APP = ToffoliGeneration(GATE_SET, N)
+                else:
+                    raise ValueError('first argument (algorithm) specified incorrectly')
+        except:
+            raise ValueError('first argument (algorithm) specified incorrectly')
+        try:
+            folder = sys.argv[3]
+        except:
+            print('folder not specified, using default directory')
+            folder = f'out/epsrc_{sys.argv[1]}_{N}qubits'
+    else:
+        print('no valid parameters provided, running with pre-specified parameters')
+        APP = QFTGeneration(GATE_SET, 3)
+        folder = 'out/eprsc_optimisers'
+
+    #QFT_GEN = QFTGeneration(GATE_SET, 3)
+    experiment_instance = Experiments(APP,iterations=20,multipliers=[8],generation_count=100,
                                       test_gate_sets={'overcomplete':GATE_SET}, save_filepath=f'{folder}')
     
     experiment_instance.run_test('sorting')
-    boxplot_from_folder(f'{folder}', fitness_reference=(2**3-1)/(2**3))
+    boxplot_from_folder(f'{folder}', fitness_reference=(2**APP.qubit_count-1)/(2**APP.qubit_count))
